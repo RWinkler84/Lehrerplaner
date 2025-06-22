d2f3a7fb2fb0ed2c2594b8110e36339b2297e38d
RWinkler84 <winkler.ralf84@hotmail.de>
2025-06-22 12:54:49 +0200

    - removed php

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
 
-require_once './core/loader.php';
-require_once './vendor/autoload.php';
-
-if (isset($_GET['c']) && isset($_GET['a'])) {
-
-    if (!in_array($_GET['c'], ALLOWEDCONTROLLER)) {
-        die('invalid controller');
-    }
-    if (!in_array($_GET['a'], ALLOWEDACTIONS)) {
-        die('invalid action');
-    }
-
-    $controllerName = '\Controller\\' . ucfirst($_GET['c']) .  'Controller';
-    $action = $_GET['a'];
-    $controller = new $controllerName;
-
-    $controller->$action();
-    exit;
-}
-
 $html = file_get_contents('./View/index.html');
 
 echo $html;
