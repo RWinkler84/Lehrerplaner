952ce00569df772528b9dc92f1e11bdc9e3887b5
RWinkler84 <winkler.ralf84@hotmail.de>
2026-01-04 12:47:19 +0100

    - fixed an issue leading to false calculation of the calendar week on certain days of the year

--- js/inc/utils.js
+++ js/inc/utils.js
@@ -96,6 +96,8 @@ export default class Utils {
     //find the first thursday of the year, which marks the first calendar week
     static getFirstThirsdayOfTheYear(year) {
         let firstDay = new Date(year + '-01-01');
+        
+        firstDay.setHours(12,0,0,0);
 
         if (firstDay.getDay() != 4) {
             while (firstDay.getDay() != 4) {
