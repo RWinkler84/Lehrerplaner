8b1e4a5779fa11097750983a15e62ab0ddc15ac8
RWinkler84 <winkler.ralf84@hotmail.de>
2026-01-23 16:48:15 +0100

    -

--- js/Controller/CurriculumController.js
+++ js/Controller/CurriculumController.js
@@ -57,6 +57,7 @@ export default class CurriculumController {
 
     static closeHolidayEditor() {
         View.closeHolidayEditor();
+        
         this.renderSchoolYearCurriculumEditor();
     }
 
