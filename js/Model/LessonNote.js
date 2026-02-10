c76c5727f628ab0ae98a434279bb4b3c0a163873
RWinkler84 <winkler.ralf84@hotmail.de>
2026-02-10 19:36:30 +0100

    - fixed bug which prevented lesson Notes from moving on edge cases

--- js/Model/LessonNote.js
+++ js/Model/LessonNote.js
@@ -148,7 +148,7 @@ export default class LessonNote extends AbstractModel {
         const allAffectedNotes = [];
 
         allLessonNotes.forEach(note => {
-            if (new Date(note.date).setHours(12) < new Date().setHours(12)) return;
+            if (new Date(note.date).setHours(12,0,0,0) < new Date().setHours(12,0,0,0)) return;
             if (note.fixedDate) return;
             
             allAffectedNotes.push(note);
