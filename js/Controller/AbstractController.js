301686734517281cccd997f0a972087cef500f54
RWinkler84 <winkler.ralf84@hotmail.de>
2026-08-13 18:59:50 +0200

    - fixed faulty function name

--- js/Controller/AbstractController.js
+++ js/Controller/AbstractController.js
@@ -222,7 +222,7 @@ export default class AbstractController {
             return false;
         }
 
-        View.toggleRevocationDialogButtons('sending');
+        View.toggleSupportDialogButtons('sending');
 
         let db = new Model;
         let result = await db.sendSupportTicket(formData);
